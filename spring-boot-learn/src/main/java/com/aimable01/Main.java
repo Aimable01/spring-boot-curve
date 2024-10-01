926c336d14f2539c27cb9511f677dad2b6ab08c4
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-01 06:32:46 +0200

    feat: usage of json with records

--- spring-boot-learn/src/main/java/com/aimable01/Main.java
+++ spring-boot-learn/src/main/java/com/aimable01/Main.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -13,7 +16,30 @@ public class Main {
     }
 
     @GetMapping("/")
-    public String greet(){
-        return "Hello";
+    public GreetResponse greet(){
+        return new GreetResponse(
+                "Hello",
+                List.of("Java","Golang","Javascript"),
+                new Person("Alex",23,43_000)
+        );
+    }
+
+    record Person(String name, int age, double savings){}
+
+    record GreetResponse(
+        String greet,
+        List<String> favProgrammingLanguages,
+        Person person
+    ){}
+
+    // to practice usage of records
+    record Car(String name, int kilos){}
+    record StartCar(boolean on,Car car){}
+
+    @GetMapping("/car")
+    public StartCar startCar(){
+        return new StartCar(
+                true,new Car("Maserati",45_000)
+        );
     }
 }
