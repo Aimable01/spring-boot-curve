9a58b393f86b86a9bb6ee19fa76532561e6331e9
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-02 20:31:11 +0200

    feat: delete customer

--- spring-boot-learn/src/main/java/com/aimable01/Main.java
+++ spring-boot-learn/src/main/java/com/aimable01/Main.java
@@ -67,4 +67,9 @@ public class Main {
         customer.setAge(request.age());
         customerRepository.save(customer);
     }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
 }
