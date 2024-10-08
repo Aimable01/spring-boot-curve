b82e0ae6ff3f9861e4b5f35a298b4386b2c0c57d
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-08 06:52:52 +0200

    feat: db connection && rest controller

--- spring_security/src/main/java/com/aimable01/spring_security/controller/HelloController.java
+++ spring_security/src/main/java/com/aimable01/spring_security/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.aimable01.spring_security;
+package com.aimable01.spring_security.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
