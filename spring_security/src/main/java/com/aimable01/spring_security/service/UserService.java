a1b136a038b2dee7967518a92d31ffdc5c7747c1
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-08 07:29:48 +0200

    fix: hash password with bcrypt

--- spring_security/src/main/java/com/aimable01/spring_security/service/UserService.java
+++ spring_security/src/main/java/com/aimable01/spring_security/service/UserService.java
@@ -3,6 +3,7 @@ package com.aimable01.spring_security.service;
 import com.aimable01.spring_security.model.Users;
 import com.aimable01.spring_security.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,11 @@ public class UserService {
     @Autowired
     private UserRepo repo;
 
+    // password hashing with bcrypt
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
     }
 }
