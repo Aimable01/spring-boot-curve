e4bb86ad306ea997e1388a0990885f4cbda6317e
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-09 08:45:31 +0200

    feat: jwt service

---
+++ spring_security/src/main/java/com/aimable01/spring_security/service/JwtService.java
@@ -0,0 +1,80 @@
+package com.aimable01.spring_security.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.apache.catalina.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "4cf4fbfb2f800b6d461d026a5b18cd0825943a6494620a2eb46d18fa35eece9a";
+
+    // get username
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    // validate token
+    public boolean isValid(String token, UserDetails user) {
+        String username = extractUsername(token);
+        return (username != null && username.equals(user.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+
+    // get token expiry
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    // get single claim
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+
+    // get all claims
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+
+    // generate token
+    public String generateToken(User user) {
+        String token = Jwts
+                .builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 24 *60 *60*1000))
+                .signWith(getSigninKey())
+                .compact();
+
+        return token;
+    }
+
+
+    // sign secret key
+    private SecretKey getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
