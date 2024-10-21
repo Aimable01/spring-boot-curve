6c513a08eebebeef490f5d01deac049a67b05d24
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-21 20:19:00 +0200

    feat: init && hello controller

---
+++ spring_boot_ultimate/src/main/java/com/Aimable01/spring_boot_ultimate/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.Aimable01.spring_boot_ultimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
