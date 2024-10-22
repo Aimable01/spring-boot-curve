a0fc10648fca607c2f6df9e51533dd083196110d
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-22 05:42:38 +0200

    fix: main app commandline

--- spring_boot_ultimate/src/main/java/com/Aimable01/spring_boot_ultimate/DemoApplication.java
+++ spring_boot_ultimate/src/main/java/com/Aimable01/spring_boot_ultimate/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.Aimable01.spring_boot_ultimate;
 
+import com.Aimable01.spring_boot_ultimate.services.ColourPrinter;
+import com.Aimable01.spring_boot_ultimate.services.impl.ColourPrintImpl;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+@Log
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		final ColourPrinter colourPrinter = new ColourPrintImpl();
+		log.info(colourPrinter.print());
+	}
 }
