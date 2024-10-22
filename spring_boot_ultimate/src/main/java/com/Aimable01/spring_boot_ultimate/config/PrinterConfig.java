f293fca80fd964427a56638cf4a75976078aa2d5
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-22 05:49:43 +0200

    feat: config

---
+++ spring_boot_ultimate/src/main/java/com/Aimable01/spring_boot_ultimate/config/PrinterConfig.java
@@ -0,0 +1,29 @@
+package com.Aimable01.spring_boot_ultimate.config;
+
+import com.Aimable01.spring_boot_ultimate.services.BluePrinter;
+import com.Aimable01.spring_boot_ultimate.services.GreenPrinter;
+import com.Aimable01.spring_boot_ultimate.services.RedPrinter;
+import com.Aimable01.spring_boot_ultimate.services.impl.EnglishBluePrinter;
+import com.Aimable01.spring_boot_ultimate.services.impl.EnglishGreenPrinter;
+import com.Aimable01.spring_boot_ultimate.services.impl.EnglishRedPrinter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PrinterConfig {
+
+    @Bean
+    public BluePrinter bluePrinter() {
+        return new EnglishBluePrinter();
+    }
+
+    @Bean
+    public GreenPrinter greenPrinter() {
+        return new EnglishGreenPrinter();
+    }
+
+    @Bean
+    public RedPrinter redPrinter() {
+        return new EnglishRedPrinter();
+    }
+}
