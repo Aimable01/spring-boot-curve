81692e94ff98d163a9357e4558e4c7dd141bb9cc
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-23 19:32:59 +0200

    feat: json init

---
+++ spring_boot_ultimate/json/src/test/java/com/Aimable01/json/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aimable01.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
