2a51f98edc979a6dede686addbde9828a1691c1d
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-25 14:07:56 +0200

    feat: obj to json

--- spring_boot_ultimate/json/src/test/java/com/Aimable01/json/JackstonTests.java
+++ spring_boot_ultimate/json/src/test/java/com/Aimable01/json/JackstonTests.java
@@ -1,4 +1,41 @@
 package com.Aimable01.json;
 
+import com.Aimable01.json.domain.Book;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class JackstonTests {
+
+    @Test
+    public void testThatObjectMapperCanCreateJsonFromJavaObject() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Book book = Book.builder()
+                .isbn("123-456-789")
+                .title("The River Between")
+                .author("Chinua Achebe")
+                .yearPublished("2005")
+                .build();
+
+        String result = objectMapper.writeValueAsString(book);
+        assertThat(result).isEqualTo("{\"isbn\":\"123-456-789\",\"title\":\"The River Between\",\"author\":\"Chinua Achebe\",\"yearPublished\":\"2005\"}");
+    }
+
+    @Test
+    public void testThatObjectMapperCanCreateObjectFromJavaObject() throws JsonProcessingException {
+        String json = "{\"isbn\":\"123-456-789\",\"title\":\"The River Between\",\"author\":\"Chinua Achebe\",\"yearPublished\":\"2005\"}";
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Book result = objectMapper.readValue(json, Book.class);
+        Book book = Book.builder()
+                .isbn("123-456-789")
+                .title("The River Between")
+                .author("Chinua Achebe")
+                .yearPublished("2005")
+                .build();
+
+        assertThat(result).isEqualTo(book);
+    }
 }
