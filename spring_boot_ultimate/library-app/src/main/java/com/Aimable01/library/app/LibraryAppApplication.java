2a75743534de595c612d7134f70382c44944ec7a
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-26 17:29:33 +0200

    init

---
+++ spring_boot_ultimate/library-app/src/main/java/com/Aimable01/library/app/LibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.Aimable01.library.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppApplication.class, args);
+	}
+
+}
