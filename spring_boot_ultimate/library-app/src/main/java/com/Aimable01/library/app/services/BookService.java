f996d2e86bbd06af1d75650e6dbd3c62aa5a879f
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-28 18:40:19 +0200

    feat: book find one

--- spring_boot_ultimate/library-app/src/main/java/com/Aimable01/library/app/services/BookService.java
+++ spring_boot_ultimate/library-app/src/main/java/com/Aimable01/library/app/services/BookService.java
@@ -3,10 +3,13 @@ package com.Aimable01.library.app.services;
 import com.Aimable01.library.app.domain.entities.BookEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookService {
 
     BookEntity createBook(String isbn, BookEntity book);
 
     List<BookEntity> findAll();
+
+    Optional<BookEntity> findOne(String isbn);
 }
