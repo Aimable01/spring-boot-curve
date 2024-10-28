35d085fbf135093d7b32a62d59bd5db20348eda8
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-28 18:25:44 +0200

    feat: author find one

--- spring_boot_ultimate/library-app/src/main/java/com/Aimable01/library/app/services/AuthorService.java
+++ spring_boot_ultimate/library-app/src/main/java/com/Aimable01/library/app/services/AuthorService.java
@@ -4,6 +4,7 @@ import com.Aimable01.library.app.domain.entities.AuthorEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface AuthorService {
@@ -11,4 +12,6 @@ public interface AuthorService {
     AuthorEntity createAuthor(AuthorEntity author);
 
     List<AuthorEntity> findAll();
+
+    Optional<AuthorEntity> findOne(Long id);
 }
