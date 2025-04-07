f86f1c7113a2facdbb2049a03b4d1e7867e3e960
Aimable01 <aimable.kwizera14@gmail.com>
2025-04-07 06:15:52 +0200

    [feat] spring security custom config

---
+++ springSecEx/src/main/java/com/aimable/springSecEx/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.aimable.springSecEx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // hey ignore the defaults
+public class SecurityConfig {
+
+    // our security filter chain spring will follow
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
+}
