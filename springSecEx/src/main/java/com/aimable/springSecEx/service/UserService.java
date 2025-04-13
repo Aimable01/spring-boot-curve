f22b44c2fbfd25cf349b7936e23a135cd1a51a87
Aimable01 <aimable.kwizera14@gmail.com>
2025-04-13 13:07:08 +0200

    feat: jwt user auth

--- springSecEx/src/main/java/com/aimable/springSecEx/service/UserService.java
+++ springSecEx/src/main/java/com/aimable/springSecEx/service/UserService.java
@@ -3,6 +3,9 @@ package com.aimable.springSecEx.service;
 import com.aimable.springSecEx.model.Users;
 import com.aimable.springSecEx.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +15,24 @@ public class UserService {
     @Autowired
     private UserRepo repo;
 
+    @Autowired
+    AuthenticationManager authManager;
+
     // encrypting the password
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public Users register(Users user) {
         user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated()) {
+            return "Success";
+        }
+
+        return "fail";
     }
 }
