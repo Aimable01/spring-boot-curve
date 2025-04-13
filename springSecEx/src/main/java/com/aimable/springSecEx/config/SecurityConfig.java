6bcb1d121c69efaa96927843e97fe205d3d73971
Aimable01 <aimable.kwizera14@gmail.com>
2025-04-13 14:01:03 +0200

    feat: validate jwt token && bearer auth usage

--- springSecEx/src/main/java/com/aimable/springSecEx/config/SecurityConfig.java
+++ springSecEx/src/main/java/com/aimable/springSecEx/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity // hey ignore the defaults
@@ -26,6 +27,10 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    // the jwt filter
+    @Autowired
+    private JwtFilter jwtFilter;
 
     // our security filter chain spring will follow
     @Bean
@@ -38,6 +43,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
